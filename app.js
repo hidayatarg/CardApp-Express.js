b7f2b913ed68a83553447596f36d1321431ec4e2
Hidayat arghandabi <hidayatarg@gmail.com>
2019-01-03 14:51:25 +0300

    Creating a Custom Route

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+
+//Create server
+const app = express();
+
+//Route
+app.get('/', (req, res) =>{
+    res.send('Hello express');
+});
+
+app.get('/about', (req, res) => {
+    res.send('Welcome to about page');
+});
+app.listen(3000);
